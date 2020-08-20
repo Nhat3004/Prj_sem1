7532f3eb5e3c4705e7a35660bc30ef0980b6fc54
Xows <hiamcuong1@gmail.com>
2020-08-20 14:33:56 +0700

    cuong

---
+++ Database/InsertData.sql
@@ -0,0 +1,6 @@
+use Prj1;
+
+insert into Staff(staff_name, staff_tel, staff_address, staff_email, staff_id_card, account, password, position)
+	values('Nguyễn Long Nhật', '0966452513', 'Vĩnh Phúc', 'longnhat8556@gmail.com', '02620004041', 'df7ac565f8308523e45b3b528513d8bd', '80be2a761fe62ab3f023e3f77dc9b8dd', 'Staff'),
+			('Nông Hồng Cương', '0338423581','Nạng Thơn', 'hiamcuong1@gmail.com', '082377587', '38ed25b6ec4c911283f7da1626e9cead', '7a98924465e161165a7fa692b11649f1', 'Manager');
+            
